95eef9e20c34d32220ae7fe3e39c01a36c857d3a
Israel <israelmatiasl@gmail.com>
2018-05-02 22:03:20 -0500

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare function init_app();
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  constructor() {}
+
+  ngOnInit(){
+    init_app();
+  }
+
+  goFeature(){
+    //console.log('Hola');
+    $("html, body").animate({ scrollTop: $('body').prop("scrollHeight")}, 1000);
+  }
 }
